1cea8a67c686d3ce6feeeed8bfa199966ef3511e
Jimi Loc Nguyen <jimilocn@gmail.com>
2019-05-25 01:58:12 -0500

    game is basically completed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
         // title within the popover
         title: 'Fill in the Blank!',
     });
+    console.log("BOOM");
 
     // click start to start game
 
@@ -13,8 +14,15 @@ $(document).ready(function () {
 
     var correctAnswers = 0;
     var incorrectAnswers = 0;
-    var unAnswers = 0;
+    var notAnswered = 0;
+    var questionNumber = 0;
+    var time = 15;
     var index = 0;
+    var questionsDiv = $("#question");
+    var answersDiv = $("#answers");
+    var dqNameDiv = $("#dqName");
+    var timerDiv = $("#timer");
+    var mamaRu = "<img src='assets/images/sad.png'>";
 
 
     // array of questions\
@@ -23,69 +31,220 @@ $(document).ready(function () {
             queen: "__________ Delano",
             answers: ["Adore", "Airy", "Adonis", "Althea"],
             rightAnswer: "Adore",
-            animate: ""
+            animate: "<img src='assets/images/adore.gif'>",
+            lose: "<img src='assets/images/away.gif'>",
+            name: "<img src='assets/images/delano.gif'>"
         },
         {
             queen: "__________ Act",
             answers: ["Christy", "Clara", "Courtney", "Cassie"],
             rightAnswer: "Courtney",
-            animate: ""
+            animate: "<img src='assets/images/courtney.gif'>",
+            lose: "<img src='assets/images/bye.gif'>",
+            name: "<img src='assets/images/act.gif'>"
         },
         {
             queen: "Bianca __________ Rio",
             answers: ["Des", "Del", "De", "Dio"],
             rightAnswer: "Del",
-            animate: ""
+            animate: "<img src='assets/images/bianca.gif'>",
+            lose: "<img src='assets/images/sashay.gif'>",
+            name: "<img src='assets/images/rio.gif'>"
         },
         {
             queen: "Lagaja __________",
             answers: ["Eleganza", "Elite", "Estranja", "Elaeja"],
             rightAnswer: "Estranja",
-            animate: ""
+            animate: "<img src='assets/images/laganja.gif'>",
+            lose: "<img src='assets/images/away.gif'>",
+            name: "<img src='assets/images/estranja.gif'>"
         },
         {
             queen: "Gia __________",
-            answers: ["Gun", "Gee", "Gia", "Grande"],
-            rightAnswer: "Gun",
-            animate: ""
+            answers: ["Gunn", "Gee", "Gia", "Grande"],
+            rightAnswer: "Gunn",
+            animate: "<img src='assets/images/gia.gif'>",
+            lose: "<img src='assets/images/bye.gif'>",
+            name: "<img src='assets/images/gunn.gif'>"
         },
         {
             queen: "Sharon __________",
             answers: ["Needles", "Yo'Mama", "Karen", "Tips"],
             rightAnswer: "Needles",
-            animate: ""
+            animate: "<img src='assets/images/Sharon.gif'>",
+            lose: "<img src='assets/images/sashay.gif'>",
+            name: "<img src='assets/images/needles.gif'>"
         },
         {
             queen: "Bob The __________ Queen",
             answers: ["Beauty", "Dairy", "Drag", "Celestial"],
             rightAnswer: "Drag",
-            animate: ""
+            animate: "<img src='assets/images/bob.gif'>",
+            lose: "<img src='assets/images/away.gif'>",
+            name: "<img src='assets/images/queen.gif'>"
         },
         {
             queen: "Plastique __________",
             answers: ["Recycle", "Bag", "Tiara", "Barbie"],
             rightAnswer: "Tiara",
-            animate: ""
+            animate: "<img src='assets/images/plastique.gif'>",
+            lose: "<img src='assets/images/bye.gif'>",
+            name: "<img src='assets/images/tiara.gif'>"
         },
         {
             queen: "Trixie __________",
             answers: ["Mattel", "Mixie", "Madeline", "Mix"],
             rightAnswer: "Mattel",
-            animate: ""
+            animate: "<img src='assets/images/trixie.gif'>",
+            lose: "<img src='assets/images/sashay.gif'>",
+            name: "<img src='assets/images/mattel.gif'>"
         },
-    ]
+        {
+            queen: "Alyssa __________",
+            answers: ["Matthews", "Edwards", "Smith", "Jones"],
+            rightAnswer: "Edwards",
+            animate: "<img src='assets/images/alyssa.gif'>",
+            lose: "<img src='assets/images/away.gif'>",
+            name: "<img src='assets/images/edwards.gif'>"
+        },
+    ];
 
-    function showTrivia() {
-        $("#question").html(questions[index].queen);
-        for (var i = 0; i < questions[index].answers.length; i++) {
-            var answerButton = $("<button>");
-            answerButton.html(questions[index].answers[i]);
-            $("#answers").append(answerButton);
+    function win() {
+        correctAnswers++;
+        questionNumber++;
+        var queenGif = $(questions[index].animate);
+        questionsDiv.html(queenGif);
+        dqNameDiv.html(questions[index].name);
+        answersDiv.empty();
+        timerDiv.empty();
+        clearInterval(clock);
+        setTimeout(nextQuestion, 4000);
+    };
+
+    function lose() {
+        incorrectAnswers++;
+        questionNumber++;
+        var loseGif = $(questions[index].lose);
+        questionsDiv.html(loseGif);
+        dqNameDiv.html(mamaRu);
+        answersDiv.empty();
+        timerDiv.empty();
+        clearInterval(clock);
+        setTimeout(nextQuestion, 4000);
+    };
+
+    function unanswered() {
+        notAnswered++;
+        questionNumber++;
+        var loseGif = $(questions[index].lose);
+        questionsDiv.html(loseGif);
+        dqNameDiv.html(mamaRu);
+        answersDiv.empty();
+        timerDiv.empty();
+        clearInterval(clock);
+        setTimeout(nextQuestion, 4000);
+
+    }
+
+    function runTime() {
+        timerDiv.empty();
+        clock = setInterval(countDown, 1000);
+        function countDown() {
+            timerDiv.html("<h2>You have " + time + " left</h2>");
+
+            if (time > 0) {
+                time--;
+            };
+
+            if (time === 0) {
+                clearInterval(clock);
+                unanswered();
+            };
+
         }
     }
 
+    function nextQuestion() {
+        if (questionNumber < questions.length) {
+            time = 15;
+            runTime();
+            index++;
+            questionsDiv.empty();
+            answersDiv.empty();
+            dqNameDiv.empty();
+            showTrivia();
+        }
 
+        else {
+            results();
+        }
+    };
+
+    function results() {
+        dqNameDiv.empty();
+        questionsDiv.empty();
+        answersDiv.empty();
+        timerDiv.empty();
+        answersDiv.html("correct answers: " + correctAnswers + "<br>");
+        answersDiv.append("incorrect answers: " + incorrectAnswers + "<br>");
+        answersDiv.append("unanswered: " + notAnswered);
+        var resetButton = $("<button>");
+        resetButton.html("<h1> Try Again <h1>");
+        timerDiv.html(resetButton);
+        resetButton.on("click", function () {
+            resetAll();
+        });
+    };
+    function resetAll() {
+        dqNameDiv.empty();
+        questionsDiv.empty();
+        answersDiv.empty();
+        timerDiv.empty();
+        correctAnswers = 0;
+        incorrectAnswers = 0;
+        notAnswered = 0;
+        questionNumber = 0;
+        time = 15;
+        index = 0;
+        showTrivia();
+        runTime();
+    }
+
+    function showTrivia() {
+        questionsDiv.html(questions[index].queen);
+
+        for (var i = 0; i < questions[index].answers.length; i++) {
+            var answerButton = $("<button>");
+            answerButton.html(questions[index].answers[i]);
+            answerButton.attr("id", questions[index].answers[i]);
+            answersDiv.append(answerButton);
+
+            $(answerButton).on("click", function () {
+
+                var userChoice = $(this).attr("id");
+                console.log(userChoice);
+
+
+                if (userChoice === questions[index].rightAnswer) {
+                    win();
+                }
+
+                else {
+                    lose();
+                }
+            });
+        }
+
+    };
+
+
+var startButton = $("<button>");
+startButton.html("<h1>Click to Start</h1>");
+timerDiv.html(startButton);
+startButton.on("click", function() {
+    runTime();
     showTrivia();
+});
 
 
 });
